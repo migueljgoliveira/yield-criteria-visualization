ccfbf5c827af66e398a5d9508a40c5c85f73c91a
Miguel G. Oliveira <37053606+migueljgoliveira@users.noreply.github.com>
2021-02-03 18:22:59 +0000

    removed Hill 1990

--- ummdp/ummdp_yfunc_hill90.bat
+++
@@ -1,8 +0,0 @@
-ECHO OFF
-cls
-
-del ummdp_yfunc_hill90.pyd >nul
-
-python -m numpy.f2py -c -m ummdp_yfunc_hill90 ummdp_yfunc_hill90.f --fcompiler=intelvem --opt=/heap-arrays:0 1> ummdp_yfunc_hill90.log 2>&1
-
-rename ummdp_yfunc_hill90.*.pyd ummdp_yfunc_hill90.pyd >nul
